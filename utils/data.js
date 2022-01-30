2f855d3ec791618cd8347f8073558cfb4f07d649
Andrew Carroll <andrewc411@yahoo.com>
2022-01-30 17:12:58 -0500

    added seed and data

---
+++ utils/data.js
@@ -0,0 +1,26 @@
+const usernames = [
+    'andrewmagic',
+    'tinytim',
+    'omicron',
+    'moana',
+    'deadlysnowman'
+];
+
+const emails = [
+    'andrewmagic@email.com',
+    'tinytim@email.com',
+    'omicron@email.com',
+    'moana@email.com',
+    'deadlysnowman@email.com'
+];
+
+
+const thoughts = [
+    'This is me thinking of thoughts',
+    'Wow that was crazy',
+    'Maybe I will do it tomorrow',
+    'Mmmmm that is good coffee',
+    'Kill all humans'
+]
+
+module.exports = { usernames, emails, thoughts };
---
+++ utils/seed.js
@@ -0,0 +1,54 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { usernames, emails, thoughts } = require('./data');
+
+
+const user = [];
+const userThought = [];
+insertUsers();
+insertThought();
+
+
+function insertUsers(){
+    for(let i=0; i < usernames.length; i++) {
+        const userObj = {
+            username: usernames[i],
+            email: emails[i]
+        }
+        user.push(userObj)
+    }
+};
+
+function insertThought(){
+    for(let i=0; i < usernames.length; i++) {
+        const thoughtObj = {
+            thoughtName: thoughts[i],
+            username: usernames[i],
+        }
+        userThought.push(thoughtObj)
+    }
+};
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  console.log(userThought)
+
+  
+  await User.deleteMany({});
+
+  
+  await Thought.deleteMany({});
+
+  await User.insertMany(user);
+
+  await Thought.insertMany(userThought);
+
+  console.table(user);
+  console.table(userThought);
+  console.info('Seeding complete!');
+  process.exit(0);
+
+
+});
